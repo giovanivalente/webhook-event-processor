27c20724af30b6715067decdb42cff24ae5bbf0d
valnog <nogueiravalente@gmail.com>
2025-05-06 19:52:06 -0300

    fix: BaseEnum returning generator instead tuple

--- realmate_challenge/shared/enum.py
+++ realmate_challenge/shared/enum.py
@@ -4,4 +4,4 @@ from enum import Enum
 class BaseEnum(str, Enum):
     @classmethod
     def choices(cls):
-        return ((item.value, item.name.title()) for item in cls)
+        return tuple((item.value, item.name.title()) for item in cls)
